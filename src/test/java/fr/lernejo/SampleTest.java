82173b24158aef8ebee68e8c89d1142e7cab2b08
Kachouti Mohamed <kachouti36@gmail.com>
2021-11-30 14:12:32 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,27 @@
+package fr.lernejo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest  {
+
+    @Test
+    void dividing_by_zero_should_produce_an_exception() {
+        int dividend = 10;
+        int divisor = 0;
+        Assertions.assertThatExceptionOfType(DivisionByZeroException.class)
+            .isThrownBy(() -> Sample.divide(dividend, divisor));
+    }
+
+    @Test
+    void dividing_10_by_2_should_produce_5() {
+        int dividend = 10; // (1)
+        int divisor = 2;
+        int quotient = Sample.divide(dividend, divisor); // (2)
+        Assertions.assertThat(quotient).as("quotient of 10 / 2")
+            .isEqualTo(5); // (3)
+    }
+
+}
